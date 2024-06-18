b12d94f8858ab3482e4508df5dd33134a4479941
abudi6 <aazzouni@ymail.com>
2024-06-18 14:26:16 +0800

    manage dar page for admin

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,6 +3,10 @@ use App\Http\Controllers\DailyAccomplishmentFormController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ManageDarController;
+
+
+///////view routes/////////////////////////////////////////
 
 Route::get('/', function () {
     return view('login');
@@ -24,12 +28,20 @@ Route::get('/admin/manage-interns', function () {
     return view('management/manage-interns');
 })->name('manage-interns');
 
+Route::get('/admin/manage-dars', function () {
+    return view('management/manage-dars');
+})->name('manage-dars');
+
+///////////////////////////////////////////////////////////
+
+
+//route for login
 Route::post('/login', [LoginController::class, 'login'])->name('login');
-
-// Register route for POST requests
+//route for registering users via admin
 Route::post('/register', [AdminController::class, 'register'])->name('register');
-
-// Route for managing interns (GET request)
+//route for displaying interns in manage interns page
 Route::get('admin/manage-interns', [AdminController::class, 'manageInterns'])->name('manage-interns');
-
+//route for creating new dars by the interns
 Route::post('/submit-daily-report', [DailyAccomplishmentFormController::class, 'submit'])->name('submit.daily.report');
+//route for displaying dars for the admin page
+Route::get('admin/manage-dars', [ManageDarController::class, 'index'])->name('manage-dars.index');
